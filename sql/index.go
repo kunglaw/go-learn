1f5655ff4d2adc251dc1aa1522d02c0e1835da23
ariesdimasy <ariesdimasy@gmail.com>
2023-06-15 15:28:03 +0700

    update

--- sql/index.go
+++ sql/index.go
@@ -64,6 +64,53 @@ func sqlQuery() {
 	}
 }
 
+func sqlPrepare() {
+	db, err := connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	stmt, err := db.Prepare("SELECT name, grade from tb_student WHERE id = ?")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	var result1 = student{}
+	stmt.QueryRow("E001").Scan(&result1.name, &result1.grade)
+	fmt.Printf("name :%s \n grade :%d \n ", result1.name, result1.grade)
+
+	var result2 = student{}
+	stmt.QueryRow("B001").Scan(&result2.name, &result2.grade)
+	fmt.Printf("name :%s \n grade :%d \n ", result2.name, result2.grade)
+}
+
+func sqlQueryRow() {
+	var db, err = connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	var result = student{}
+
+	var id string = "E001"
+
+	err = db.QueryRow("SELECT name, grade FROM tb_student WHERE id = ? ", id).
+		Scan(&result.name, &result.grade)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("name: %s \n grade: %d \n", result.name, result.grade)
+}
+
 func main() {
-	sqlQuery()
+	// sqlQuery()
+	// sqlQueryRow()
+	sqlPrepare()
 }
--- struct/substruct/index.go
+++ struct/substruct/index.go
@@ -14,6 +14,9 @@ type student struct {
 
 func main() {
 	var p1 = person{name: "Dimas", age: 30}
+	var p2 = person{name: "Kunglaw", age: 32}
+	p2.name = "Babe"
+	fmt.Println(p2)
 	var s1 = student{person: p1, grade: 2}
 
 	fmt.Println(s1)
